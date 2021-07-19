9fbb5f221b70d13135f6ab903a32288376cee767
Borguuh <abdrahmanadewale@gmail.com>
2021-07-19 16:15:46 +0100

    mailer edit

--- services/Mailer.js
+++ services/Mailer.js
@@ -7,7 +7,7 @@ class Mailer extends helper.Mail {
     super();
 
     this.sgApi = sendgrid(keys.sendGridKey);
-    this.from_email = new helper.Email("jerry.yip@gmail.com");
+    this.from_email = new helper.Email("abdrahmanadewale@gmail.com");
     this.subject = subject;
     this.body = new helper.Content("text/html", content);
     this.recipients = this.formatAddresses(recipients);
